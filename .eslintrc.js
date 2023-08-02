6804e33a60a58ed16f0545431a8c8dafc35f41f3
Sergey Grabak <seventyseventh@gmail.com>
2023-08-03 01:12:54 +0200

    chore: add husky and update linter

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+module.exports =
+{
+  extends: [
+    'plugin:@next/next/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  root: true,
+  rules: {
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'no-magic-numbers': 'off',
+    '@typescript-eslint/no-magic-numbers': [
+      'error',
+      {
+        ignoreEnums: true
+      }
+    ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/array-type': [
+      'error',
+      {
+        'default': 'array'
+      }
+    ],
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      {
+        'accessibility': 'explicit',
+        'overrides': {
+          'accessors': 'explicit',
+          'constructors': 'off',
+          'methods': 'explicit',
+          'properties': 'explicit',
+          'parameterProperties': 'explicit'
+        }
+      }
+    ],
+    'max-lines-per-function': [
+      'error',
+      {
+        'max': 60,
+        'skipComments': true,
+        'skipBlankLines': true
+      }
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    'no-debugger': 'off',
+    'no-console': 0,
+    'class-methods-use-this': 'off'
+  }
+}
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  name: string
-}
+interface Data {
+  name: string;
+};
+
+enum ResponseCode {
+  'OK' = 200,
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+): void {
+  res.status(ResponseCode.OK).json({ name: 'John Doe' });
 }
