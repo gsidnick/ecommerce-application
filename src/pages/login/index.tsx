8f7628779a46fcc4b4c1e933fef9ee7d46db893d
Sergey Grabak <seventyseventh@gmail.com>
2023-08-14 00:52:57 +0200

    feat: add eye to reveal password

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,11 +2,13 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { useRouter } from 'next/router';
 import { NextPage } from 'next';
-import { ReactElement, useState } from 'react';
+import { ReactElement, useRef, useState } from 'react';
 import { setAuthState } from '../../store/slices/authSlice';
 import { LoginProps } from '../api/user/login';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
 import Loader from '../../components/ui/loader/Loader';
+import EyePassVisible from '../../components/ui/icons/EyePassVisible';
+import EyePass from '../../components/ui/icons/EyePass';
 
 const MIN_PASSWORD_LENGTH = 8;
 
@@ -15,6 +17,9 @@ const LoginPage: NextPage = () => {
   const router = useRouter();
   const dispatch = useAppDispatch();
   const [isLoading, setIsLoading] = useState(false);
+  const [visionPass, setVisionPass] = useState<boolean>(false);
+
+  const passwordRef = useRef<HTMLInputElement>(null);
 
   const formik = useFormik({
     initialValues: {
@@ -105,9 +110,19 @@ const LoginPage: NextPage = () => {
     });
   };
 
+  const toggleVisionPass = (): void => {
+    setVisionPass((prev) => {
+      if (passwordRef.current) {
+        passwordRef.current.type = !prev ? 'text' : 'password';
+      }
+
+      return !prev;
+    });
+  };
+
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="p-6 rounded w-96 bg-background-main shadow-modal">
+    <div className="flex h-screen items-center justify-center">
+      <div className="w-96 rounded bg-background-main p-6 shadow-modal">
         <h1 className="mb-4 text-2xl font-semibold text-white">Login</h1>
         <form onSubmit={formik.handleSubmit}>
           <div className="mb-4">
@@ -118,27 +133,38 @@ const LoginPage: NextPage = () => {
               value={formik.values.email}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              className="w-full p-2 text-white border rounded-md border-neutral-800 bg-background-main focus:border-neutral-500 focus:outline-none"
+              className="w-full rounded-md border border-neutral-800 bg-background-main p-2 text-white focus:border-neutral-500 focus:outline-none"
             />
-            {formik.touched.email && formik.errors.email && (
+            {formik.errors.email && (
               <div className="text-red-500">{formik.errors.email}</div>
             )}
           </div>
-          <div className="mb-4">
+          <div className="relative mb-4">
             <input
               type="password"
               placeholder="Password"
               name="password"
+              ref={passwordRef}
               value={formik.values.password}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
-              className="w-full p-2 text-white border rounded-md border-neutral-800 bg-background-main focus:border-neutral-500 focus:outline-none"
+              className="w-full rounded-md border border-neutral-800 bg-background-main p-2 text-white focus:border-neutral-500 focus:outline-none"
             />
-            {formik.touched.password && formik.errors.password && (
+            <button
+              type="button"
+              className={`absolute right-3 top-3 text-white transition-transform ease-in-out ${
+                visionPass ? 'rotate-0' : 'rotate-180'
+              }`}
+              onClick={toggleVisionPass}
+            >
+              {visionPass ? <EyePassVisible /> : <EyePass />}
+            </button>
+
+            {formik.errors.password && (
               <div className="text-red-500">
                 {' '}
                 {formik.errors.password}
-                <ul className="mt-1 ml-6 text-sm text-red-500 list-disc">
+                <ul className="ml-6 mt-1 list-disc text-sm text-red-500">
                   {renderValidationMessage(
                     'length',
                     formik.values.password.length >= MIN_PASSWORD_LENGTH,
@@ -173,7 +199,7 @@ const LoginPage: NextPage = () => {
               </div>
             )}
           </div>
-          <div className="flex items-center mb-4">
+          <div className="mb-4 flex items-center">
             <label className="text-white" htmlFor="rememberMe">
               <input
                 type="checkbox"
@@ -201,7 +227,7 @@ const LoginPage: NextPage = () => {
           <button
             type="button"
             onClick={handleRegistration}
-            className="w-full py-2 mt-2 text-white bg-gray-600 rounded-md hover:bg-gray-700"
+            className="mt-2 w-full rounded-md bg-gray-600 py-2 text-white hover:bg-gray-700"
           >
             Sign up
           </button>
