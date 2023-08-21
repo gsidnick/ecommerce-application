0034a86f98602c917d3b00680c9b3018f92e42f0
Kirill Nazarov <deeplogic2000@gmail.com>
2023-08-21 18:50:15 +0300

    fix: removed max-lines-per-function rule in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,14 +52,6 @@ module.exports = {
         },
       },
     ],
-    'max-lines-per-function': [
-      'error',
-      {
-        max: 150,
-        skipComments: true,
-        skipBlankLines: true,
-      },
-    ],
     '@typescript-eslint/explicit-function-return-type': 'error',
     '@typescript-eslint/no-unnecessary-type-assertion': 'error',
     '@typescript-eslint/no-non-null-assertion': 'error',
