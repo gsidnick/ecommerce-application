bc07ea6b3d668507978e72f457d2c5dfe0fdd58b
Nick GS <gsidnick@gmail.com>
2023-08-23 17:50:55 +0300

    fix: recover white text on billing fields

--- src/components/CustomInput/CustomBillingInput/index.tsx
+++ src/components/CustomInput/CustomBillingInput/index.tsx
@@ -75,7 +75,7 @@ const CustomBillingInput: FC<InputProps> = (props) => {
         className={
           type === 'checkbox'
             ? 'rounded-md border border-neutral-800 bg-background-main p-2 text-white focus:border-neutral-500 focus:outline-none'
-            : 'w-full rounded-md border border-neutral-800 bg-background-main p-2 pl-3 pr-3 focus:border-neutral-500 focus:outline-none'
+            : 'w-full rounded-md border border-neutral-800 bg-background-main p-2 pl-3 pr-3 text-white focus:border-neutral-500 focus:outline-none'
         }
         onChange={handleOnChangeBillingAddress}
       />
