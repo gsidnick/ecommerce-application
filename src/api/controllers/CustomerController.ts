040a5f02e88e1439638193bd71999f30bd581f4b
Nick GS <gsidnick@gmail.com>
2023-08-27 23:40:38 +0300

    feat: add updateCustomer method

--- src/api/controllers/CustomerController.ts
+++ src/api/controllers/CustomerController.ts
@@ -1,5 +1,6 @@
 import { ClientResponse } from '@commercetools/sdk-client-v2';
 import { Customer } from '@commercetools/platform-sdk';
+import { MyCustomerUpdate } from '@commercetools/platform-sdk/dist/declarations/src/generated/models/me';
 import CustomerRepository from '@/api/repositories/CustomerRepository';
 import {
   IApiLoginResult,
@@ -32,6 +33,12 @@ class CustomerController {
 
   public async getCustomer(): Promise<ClientResponse<Customer> | undefined> {
     return this.customerRepository.getCustomer();
+  }
+
+  public async updateCustomer(
+    data: MyCustomerUpdate
+  ): Promise<ClientResponse<Customer>> {
+    return this.customerRepository.updateCustomer(data);
   }
 }
 
