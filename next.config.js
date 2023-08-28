8f0ad5252b44ac2cb2b55e010762009cc91eb680
Kirill Nazarov <deeplogic2000@gmail.com>
2023-08-28 20:59:31 +0300

    feat: (52) add product card ui

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '607c34ad0a5bf735fdf7-ec12c9005026a0c273dadf2c3ac4444b.ssl.cf3.rackcdn.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
---
+++ src/components/ProductCard/index.tsx
@@ -0,0 +1,70 @@
+import { FC } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import type { StaticImageData } from 'next/image';
+import {
+  PRODUCT_DESCRIPTION_SLICE_FROM,
+  PRODUCT_DESCRIPTION_SLICE_TO,
+} from '@/constants';
+
+import styles from './styles.module.css';
+
+interface ProductCardProps {
+  id: string;
+  //   img: string;
+  img: StaticImageData;
+  name: string;
+  model: string;
+  description: string;
+  price: string;
+  oldPrice: string;
+  currency: string;
+}
+
+const ProductCard: FC<ProductCardProps> = (props) => {
+  const { id, img, name, model, description, price, oldPrice, currency } =
+    props;
+
+  const briefDescription = `${description
+    .slice(PRODUCT_DESCRIPTION_SLICE_FROM, PRODUCT_DESCRIPTION_SLICE_TO)
+    .trim()}...`;
+
+  return (
+    <Link href={`/product/${id}`}>
+      <div id={id} className={styles.cardWrapper}>
+        <div className={`${styles.container} text-white`}>
+          <Image
+            src={img}
+            alt={name}
+            className={styles.image}
+            width={220}
+            height={500}
+          />
+          <div className={`${styles.infoWrapper}`}>
+            <p className={styles.title}>{name}</p>
+            <p className={styles.modelName}>{model}</p>
+            <p className={styles.description}>{briefDescription}</p>
+            <div className="flex justify-between">
+              <div>
+                <p className={`${styles.oldPciceWrapper} text-sm line-through`}>
+                  {currency} {oldPrice}
+                </p>
+                <p className={`${styles.pciceWrapper} text-lg font-bold`}>
+                  {currency} {price}
+                </p>
+              </div>
+              <div
+                role="button"
+                className={`${styles.button} rounded-md bg-slate-500 text-white`}
+              >
+                Add to cart
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductCard;
