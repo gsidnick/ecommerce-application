1bdfe011b7db3e679bc067cf0645d22d221f67fb
Kirill Nazarov <deeplogic2000@gmail.com>
2023-08-30 18:41:39 +0300

    feat: (52) add filtered products section, add pagination

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '607c34ad0a5bf735fdf7-ec12c9005026a0c273dadf2c3ac4444b.ssl.cf3.rackcdn.com',
+        hostname: 'images.ctfassets.net',
         port: '',
         pathname: '/**',
       },
