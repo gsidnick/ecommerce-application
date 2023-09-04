1eb4d4913f75eb456682b85a675e6cd4d7dab353
Kirill Nazarov <deeplogic2000@gmail.com>
2023-09-05 00:47:37 +0300

    fix: (52) fix styles, delete unneccessary code

--- src/components/SideBar/SideBarElement/index.tsx
+++ src/components/SideBar/SideBarElement/index.tsx
@@ -1,14 +1,7 @@
 import { ReactElement } from 'react';
-// import { ICategory } from '../../pages/catalog/categoriesData';
 
-// interface ISideBarElementProps {
-//   id: string;
-//   name: string;
-//   subcategories?: ICategory[];
-// }
 const SideBar = (): ReactElement => {
-  console.log('SideBar');
-
+console.log('SideBar Element');
   return (
     <div>
       <span>SidebBar Element</span>
