b7fdb72fdd1db9d99de02db7ebc3636f1c4e2e7f
Nick GS <gsidnick@gmail.com>
2023-09-14 01:02:08 +0300

    refactor: change AuthGate with anonymous customer

--- src/components/AuthGate/index.tsx
+++ src/components/AuthGate/index.tsx
@@ -1,8 +1,18 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { toast } from 'react-toastify';
 import { ERoute } from '@/data/routes';
-import { useAppStore } from '@/hooks/useAppStore';
+import { useAppDispatch } from '@/hooks/useAppDispatch';
+import { useAppSelector } from '@/hooks/useAppSelector';
+import {
+  selectAuthState,
+  setExpirationTime,
+  setRefreshToken,
+  setToken,
+} from '@/store/slices/authSlice';
+import CustomerController from '@/api/controllers/CustomerController';
+import CartController from '@/api/controllers/CartController';
+import { IMMEDIATE_INVOKE } from '@/constants';
 
 export const AuthGate = ({
   children,
@@ -10,27 +20,49 @@ export const AuthGate = ({
   children: ReactElement;
 }): ReactElement | null => {
   const router = useRouter();
-  const store = useAppStore();
-  const currentState = store.getState();
+  const dispatch = useAppDispatch();
+  const { authState, token: tokenState } = useAppSelector(selectAuthState);
+  const customerController = new CustomerController();
+  const cartController = new CartController();
 
-  const isAuth = currentState.auth.authState;
+  useEffect(() => {
+    const anonymous = setTimeout(() => {
+      if (!authState && !tokenState) {
+        customerController
+          .createAnonymousCustomer()
+          .then((response) => {
+            const { token, refreshToken, expirationTime } = response;
+            dispatch(setToken(token));
+            dispatch(setRefreshToken(refreshToken ?? ''));
+            dispatch(setExpirationTime(expirationTime));
+            void cartController.createCart();
+          })
+          .catch(console.error);
+      }
+    }, IMMEDIATE_INVOKE);
 
-  if (
-    isAuth &&
-    (window.location.pathname === (ERoute.login as string) ||
-      window.location.pathname === (ERoute.signup as string))
-  ) {
-    router
-      .push(ERoute.home)
-      .then(() => {
-        toast.info('Already logged in, redirect to main page');
-      })
-      .catch(() => {
-        toast.error('Error while redirecting to home page');
-      });
+    const redirect = setInterval(() => {
+      if (
+        authState &&
+        (window.location.pathname === (ERoute.login as string) ||
+          window.location.pathname === (ERoute.signup as string))
+      ) {
+        router
+          .push(ERoute.home)
+          .then(() => {
+            toast.info('Already logged in, redirect to main page');
+          })
+          .catch(() => {
+            toast.error('Error while redirecting to home page');
+          });
+      }
+    }, IMMEDIATE_INVOKE);
 
-    return null;
-  }
+    return () => {
+      clearInterval(anonymous);
+      clearInterval(redirect);
+    };
+  }, []);
 
   return children;
 };
