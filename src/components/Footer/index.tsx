98ec955d7da9bc6aca409bdfd3b3e73136798d4d
Nick GS <gsidnick@gmail.com>
2023-09-21 02:55:28 +0300

    feat: add footer component adaptive

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,8 +3,8 @@ import RSSchoolIcon from '@/components/ui/icons/RSSchoolIcon';
 import GithubIcon from '@/components/ui/icons/GithubIcon';
 
 const Footer = (): ReactElement => (
-  <footer className="grid grid-cols-3 items-center gap-2 bg-gray-dark px-8 py-4 text-gray-400">
-    <div className="justify-self-left flex flex-row gap-2">
+  <footer className="grid grid-cols-1 items-center gap-2 bg-gray-dark px-8 py-4 text-gray-400 sm:grid-cols-3">
+    <div className="order-1 flex flex-row gap-2 justify-self-center sm:order-1 sm:justify-self-start">
       <a
         className="svg-icon block h-8 w-20 transition-colors duration-300 hover:text-white"
         href="https://rs.school/js/"
@@ -12,10 +12,10 @@ const Footer = (): ReactElement => (
         <RSSchoolIcon />
       </a>
     </div>
-    <div className="justify-self-center text-xs">
+    <div className="order-3 justify-self-center text-center text-xs sm:order-2">
       © 2023, All Rights Reserved
     </div>
-    <div className="flex flex-row gap-2 justify-self-end">
+    <div className="order-2 flex flex-row gap-2 justify-self-center sm:order-3 sm:justify-self-end">
       <a
         className="svg-icon block h-8 w-8 transition-colors duration-300 hover:text-white"
         href="https://github.com/gsidnick/"
