73b6aca2624d4d4ddb2c74c39de809eed2a83651
Nick GS <gsidnick@gmail.com>
2023-09-21 03:13:06 +0300

    feat: add styles to promocodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,12 @@
 import { ReactElement } from 'react';
-
 import MainSlider from '@/components/slider/MainSlider';
+import Promocodes from '@/components/Promocodes';
 
 function Home(): ReactElement {
   return (
     <>
       <MainSlider />
-      <div className="mt-2 ml-3">
-        Promocodes to apply:
-        <ul>
-          <li>30ALL - 30% off on everything</li>
-          <li>5BASS - 5% off on Bass guitars</li>
-          <li>10ACOUSTIC - 10% off on Acoustic guitars</li>
-        </ul>
-      </div>
+      <Promocodes />
     </>
   );
 }
