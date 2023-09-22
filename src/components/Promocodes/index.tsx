8bcfe01a7f1ea080998eb03194e7af4a1f3b8d01
Nick GS <gsidnick@gmail.com>
2023-09-22 15:21:25 +0300

    refactor: add background to promocodes

--- src/components/Promocodes/index.tsx
+++ src/components/Promocodes/index.tsx
@@ -6,7 +6,7 @@ const Promocodes = (): ReactElement => (
       Promocodes
     </div>
     <div className="mx-auto max-w-7xl px-6 lg:px-8">
-      <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+      <dl className="grid grid-cols-1 gap-x-8 gap-y-16 rounded-lg bg-gray-100 p-8 text-center lg:grid-cols-3">
         <div className="mx-auto flex max-w-xs flex-col gap-y-4">
           <dt className="text-base leading-7 text-gray-600">
             30% off on everything
